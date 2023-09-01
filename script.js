31f7a6e560a423764f69507fd14de727cf477ee1
Kenneth <kennethlorenzg@gmail.com>
2023-08-31 22:39:10 -0400

    Add function for nav open/close

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ window.addEventListener("scroll", () => {
 });
 
 document.querySelector(".currentYear").textContent = new Date().getFullYear();
+
+// nav
+const menu = document.querySelector(".menuIcon");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
